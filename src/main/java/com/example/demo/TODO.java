40c44fbbe80e1ff6e18477930fe6f2549ec61842
tchekol1 <tchekol1@umbc.edu>
2019-08-20 15:26:48 -0400

    ex302

---
+++ src/main/java/com/example/demo/TODO.java
@@ -0,0 +1,58 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class TODO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @NotNull
+@Size(min=4)
+    private String Task_Name;
+    @NotNull
+    @Size(min=4)
+    private String duedate;
+    @NotNull
+    @Size(min=4)
+  private String priority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask_Name() {
+        return Task_Name;
+    }
+
+    public void setTask_Name(String task_Name) {
+        Task_Name = task_Name;
+    }
+
+    public String getDuedate() {
+        return duedate;
+    }
+
+    public void setDuedate(String duedate) {
+        this.duedate = duedate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
